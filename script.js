0061e57dc6b66a070d84d833a57845e1a3d15270
5uplovr619 <145370202+5uplovr619@users.noreply.github.com>
2024-04-16 10:03:28 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+let btn = document.getElementById("resultbtn");
+let Qone = document.getElementById("q1");
+let Qtwo = document.getElementById("q2");
+let Qthree = document.getElementById("q3");
+let Qfour = document.getElementById("q4");
+let Qfive = document.getElementById("q5");
+let Qsix = document.getElementById("q6");
+let Aone = document.getElementById("ans1");
+let Atwo = document.getElementById("ans2");
+let Athree = document.getElementById("ans3");
+let Afour = document.getElementById("ans4");
+let Afive = document.getElementById("ans5");
+let Asix = document.getElementById("ans6");
+let resultText = document.getElementById("result");
+
+btn.addEventListener("click", btnclicked);
+
+let resultnum = 0;
+
+function btnclicked() {
+  console.log(Qone);
+  if (
+    Qone.value === "16" ||
+    Qone.value === "Sixteen" ||
+    Qone.value === "sixteen"
+  ) {
+    resultnum++;
+    Aone.innerHTML = "Correct";
+  } else {
+    Aone.innerHTML = "Incorrect";
+  }
+  if (
+    Qtwo.value == "Baby dragon" ||
+    Qtwo.value == "baby dragon" ||
+    Qtwo.value == "baby Dragon" ||
+    Qtwo.value == "Baby Dragon"
+  ) {
+    resultnum++;
+    Atwo.innerHTML = "Correct";
+  } else {
+    Atwo.innerHTML = "Incorrect";
+  }
+  if (
+    Qthree.value === "7" ||
+    Qthree.value === "seven" ||
+    Qthree.value === "Seven"
+  ) {
+    resultnum++;
+    Athree.innerHTML = "Correct";
+  } else {
+    Athree.innerHTML = "Incorrect";
+  }
+  if (
+    Qfour.value === "inferno tower" ||
+    Qfour.value === "Inferno Tower" ||
+    Qfour.value === "inferno Tower" ||
+    Qfour.value === "Inferno tower"
+  ) {
+    resultnum++;
+    Afour.innerHTML = "Correct";
+  } else {
+    Afour.innerHTML = "Incorrect";
+  }
+  if (
+    Qfive.value === "hog rider" ||
+    Qfive.value === "Hog rider" ||
+    Qfive.value === "hog Rider" ||
+    Qfive.value === "Hog Rider"
+  ) {
+    resultnum++;
+    Afive.innerHTML = "Correct";
+  } else {
+    Afive.innerHTML = "Incorrect";
+  }
+  if (
+    Qsix.value === "clash" ||
+    Qsix.value === "Clash" ||
+    Qsix.value === "coc" ||
+    Qsix.value === "Coc" ||
+    Qsix.value === "COC" ||
+    Qsix.value === "cOc"
+  ) {
+    resultnum++;
+    Asix.innerHTML = "Correct";
+  } else {
+    Asix.innerHTML = "Incorrect";
+  }
+  endpercent = Math.floor((resultnum / 6) * 100);
+  resultText.innerHTML = `${resultnum} / 6 OR ${endpercent}%`;
+  resultnum = 0;
+}
